bdb9ae40bb2e9ac62cfad4160b3ba60f3410bb69
noriva <jrmarson.nmj@gmail.com>
2020-07-10 22:40:39 -0300

    O projeto foi iniciado

---
+++ src/main/java/com/norival/cursomcvirtual/CursomcvirtualApplication.java
@@ -0,0 +1,13 @@
+package com.norival.cursomcvirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcvirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcvirtualApplication.class, args);
+	}
+
+}
